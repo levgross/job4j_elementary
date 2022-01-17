5a7af06c2b9b14d1f9061ed991dd2fb20a89a490
Grossevich Lev <levgross@gmail.com>
2022-01-17 15:40:19 +0400

    заменил условие в цикле while заменил for i на for each

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -6,9 +6,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-            for (int i = 0; i < coins.length; i++) {
-                while (money - price >= coins[i]) {
-                    rsl[size] = coins[i];
+        money -= price;
+            for (int coin : coins) {
+                while (money >= coin) {
+                    rsl[size] = coin;
                     money -= rsl[size];
                     size++;
                 }
